0aced108ae03be0570874a5b25b97a53e655835d
yilongchun <5115413@qq.com>
2015-02-09 14:05:40 +0000

    课程表优化

--- hmjz/view/KcbViewController.m
+++ hmjz/view/KcbViewController.m
@@ -25,66 +25,69 @@
 
 - (void)loadData{
     
-    NSString *leftstr = [[NSMutableString alloc] initWithString:@""];
-    NSString *rightstr = [[NSMutableString alloc] initWithString:@""];
-    if ([self.dataSource count] > 9) {
-        for (int i = 0 ; i < 9; i++) {
-            NSDictionary *data = [self.dataSource objectAtIndex:i];
-            NSString *starttime = [data objectForKey:@"startTime"];
-            NSString *content = [data objectForKey:self.weekName];
-            leftstr = [leftstr stringByAppendingString:starttime];
-            leftstr = [leftstr stringByAppendingString:@" "];
-            leftstr = [leftstr stringByAppendingString:content];
-            leftstr = [leftstr stringByAppendingString:@"\n"];
+    if (self.dataSource.count > 0) {
+        NSString *leftstr = [[NSMutableString alloc] initWithString:@""];
+        NSString *rightstr = [[NSMutableString alloc] initWithString:@""];
+        if ([self.dataSource count] > 9) {
+            for (int i = 0 ; i < 9; i++) {
+                NSDictionary *data = [self.dataSource objectAtIndex:i];
+                NSString *starttime = [data objectForKey:@"startTime"];
+                NSString *content = [data objectForKey:self.weekName];
+                leftstr = [leftstr stringByAppendingString:starttime];
+                leftstr = [leftstr stringByAppendingString:@" "];
+                leftstr = [leftstr stringByAppendingString:content];
+                leftstr = [leftstr stringByAppendingString:@"\n"];
+            }
+            for (int i = 9; i < [self.dataSource count]; i++) {
+                NSDictionary *data = [self.dataSource objectAtIndex:i];
+                NSString *starttime = [data objectForKey:@"startTime"];
+                NSString *content = [data objectForKey:self.weekName];
+                rightstr = [rightstr stringByAppendingString:starttime];
+                rightstr = [rightstr stringByAppendingString:@" "];
+                rightstr = [rightstr stringByAppendingString:content];
+                rightstr = [rightstr stringByAppendingString:@"\n"];
+            }
+        }else{
+            for (int i = 0 ; i < [self.dataSource count]; i++) {
+                NSDictionary *data = [self.dataSource objectAtIndex:i];
+                NSString *starttime = [data objectForKey:@"startTime"];
+                NSString *content = [data objectForKey:self.weekName];
+                leftstr = [leftstr stringByAppendingString:starttime];
+                leftstr = [leftstr stringByAppendingString:@" "];
+                leftstr = [leftstr stringByAppendingString:content];
+                leftstr = [leftstr stringByAppendingString:@"\n"];
+            }
         }
-        for (int i = 9; i < [self.dataSource count]; i++) {
-            NSDictionary *data = [self.dataSource objectAtIndex:i];
-            NSString *starttime = [data objectForKey:@"startTime"];
-            NSString *content = [data objectForKey:self.weekName];
-            rightstr = [rightstr stringByAppendingString:starttime];
-            rightstr = [rightstr stringByAppendingString:@" "];
-            rightstr = [rightstr stringByAppendingString:content];
-            rightstr = [rightstr stringByAppendingString:@"\n"];
+        
+        self.leftContent.text = leftstr;
+        NSMutableAttributedString * attributedString1 = [[NSMutableAttributedString alloc] initWithString:self.leftContent.text];
+        NSMutableParagraphStyle * paragraphStyle1 = [[NSMutableParagraphStyle alloc] init];
+        [paragraphStyle1 setLineSpacing:3];
+        [attributedString1 addAttribute:NSParagraphStyleAttributeName value:paragraphStyle1 range:NSMakeRange(0, [self.leftContent.text length])];
+        [self.leftContent setAttributedText:attributedString1];
+        [self.leftContent sizeToFit];
+        
+        self.rightContent.text = rightstr;
+        NSMutableAttributedString * attributedString2 = [[NSMutableAttributedString alloc] initWithString:self.rightContent.text];
+        NSMutableParagraphStyle * paragraphStyle2 = [[NSMutableParagraphStyle alloc] init];
+        [paragraphStyle2 setLineSpacing:3];
+        [attributedString2 addAttribute:NSParagraphStyleAttributeName value:paragraphStyle2 range:NSMakeRange(0, [self.rightContent.text length])];
+        [self.rightContent setAttributedText:attributedString2];
+        [self.rightContent sizeToFit];
+        CGFloat height = self.leftContent.frame.size.height;
+        if (self.rightContent.frame.size.height > height) {
+            height = self.rightContent.frame.size.height;
         }
-    }else{
-        for (int i = 0 ; i < [self.dataSource count]; i++) {
-            NSDictionary *data = [self.dataSource objectAtIndex:i];
-            NSString *starttime = [data objectForKey:@"startTime"];
-            NSString *content = [data objectForKey:self.weekName];
-            leftstr = [leftstr stringByAppendingString:starttime];
-            leftstr = [leftstr stringByAppendingString:@" "];
-            leftstr = [leftstr stringByAppendingString:content];
-            leftstr = [leftstr stringByAppendingString:@"\n"];
+        if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1){
+            if (iPhone4) {
+                height += 50;
+            }
+        }else{
+            height += 64;
         }
+        [self.conBackground setFrame:CGRectMake(self.conBackground.frame.origin.x, self.conBackground.frame.origin.y, self.conBackground.frame.size.width, height+80+44)];
     }
     
-    self.leftContent.text = leftstr;
-    NSMutableAttributedString * attributedString1 = [[NSMutableAttributedString alloc] initWithString:self.leftContent.text];
-    NSMutableParagraphStyle * paragraphStyle1 = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle1 setLineSpacing:3];
-    [attributedString1 addAttribute:NSParagraphStyleAttributeName value:paragraphStyle1 range:NSMakeRange(0, [self.leftContent.text length])];
-    [self.leftContent setAttributedText:attributedString1];
-    [self.leftContent sizeToFit];
-    
-    self.rightContent.text = rightstr;
-    NSMutableAttributedString * attributedString2 = [[NSMutableAttributedString alloc] initWithString:self.rightContent.text];
-    NSMutableParagraphStyle * paragraphStyle2 = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle2 setLineSpacing:3];
-    [attributedString2 addAttribute:NSParagraphStyleAttributeName value:paragraphStyle2 range:NSMakeRange(0, [self.rightContent.text length])];
-    [self.rightContent setAttributedText:attributedString2];
-    [self.rightContent sizeToFit];
-    CGFloat height = self.leftContent.frame.size.height;
-    if (self.rightContent.frame.size.height > height) {
-        height = self.rightContent.frame.size.height;
-    }
-    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1){
-        if (iPhone4) {
-            height += 50;
-        }
-    }else{
-        height += 64;
-    }
-    [self.conBackground setFrame:CGRectMake(self.conBackground.frame.origin.x, self.conBackground.frame.origin.y, self.conBackground.frame.size.width, height+80+44)];
 }
 
 - (void)viewDidAppear:(BOOL)animated

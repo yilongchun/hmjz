57775d645ad7c713f6d62f3343878e414cf433a2
yilongchun <5115413@qq.com>
2015-03-30 09:48:17 +0000

    去掉版本更新提示

--- hmjz/shezi/ShezhiViewController.m
+++ hmjz/shezi/ShezhiViewController.m
@@ -108,7 +108,7 @@
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if (section == 0) {
-        return 4;
+        return 2;
     }
     return 1;
 }

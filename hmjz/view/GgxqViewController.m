caec14382eed0e1fdb7035cbe7dc73fda2167db3
yilongchun <5115413@qq.com>
2015-08-17 10:16:49 +0800

    公告详情修改

--- hmjz/view/GgxqViewController.m
+++ hmjz/view/GgxqViewController.m
@@ -13,7 +13,6 @@
 #import "MBProgressHUD.h"
 #import "ContentCell.h"
 #import "PinglunTableViewCell.h"
-#import "SRRefreshView.h"
 #import "IQKeyboardManager.h"
 #import "MJRefresh.h"
 
@@ -25,7 +24,7 @@
     NSNumber *rows;
 }
 
-@property (nonatomic, strong) SRRefreshView         *slimeView;
+
 @end
 
 @implementation GgxqViewController
@@ -255,9 +254,6 @@
     if ([mytableview respondsToSelector:@selector(setLayoutMargins:)]) {
         [mytableview setLayoutMargins:UIEdgeInsetsZero];
     }
-//   [self.mytableview addSubview:self.slimeView];
-    
-//    [self loadData];
     
     // 添加下拉刷新控件
     self.mytableview.header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
@@ -278,66 +274,6 @@
     [super viewDidDisappear:animated];
     [IQKeyboardManager sharedManager].enable = YES;
     [IQKeyboardManager sharedManager].enableAutoToolbar = YES;
-}
-
-#pragma mark - getter
-
-//- (SRRefreshView *)slimeView
-//{
-//    if (!_slimeView) {
-//        _slimeView = [[SRRefreshView alloc] init];
-//        _slimeView.delegate = self;
-//        _slimeView.upInset = 0;
-//        _slimeView.slimeMissWhenGoingBack = YES;
-//        _slimeView.slime.bodyColor = [UIColor grayColor];
-//        _slimeView.slime.skinColor = [UIColor grayColor];
-//        _slimeView.slime.lineWith = 1;
-//        _slimeView.slime.shadowBlur = 4;
-//        _slimeView.slime.shadowColor = [UIColor grayColor];
-//        _slimeView.backgroundColor = [UIColor whiteColor];
-//    }
-//    
-//    return _slimeView;
-//}
-
-- (void)loadData{
-    [HUD show:YES];
-    NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    NSString *userid = [userDefaults objectForKey:@"userid"];
-    [dic setValue:userid forKey:@"userId"];
-    [dic setValue:self.tnid forKey:@"tnid"];
-    
-    MKNetworkOperation *op = [engine operationWithPath:@"/Notice/findbyid.do" params:dic httpMethod:@"GET"];
-    [op addCompletionHandler:^(MKNetworkOperation *operation) {
-//        NSLog(@"[operation responseData]-->>%@", [operation responseString]);
-        NSString *result = [operation responseString];
-        NSError *error;
-        NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:[result dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
-        if (resultDict == nil) {
-            NSLog(@"json parse failed \r\n");
-        }
-        NSNumber *success = [resultDict objectForKey:@"success"];
-        //        NSString *msg = [resultDict objectForKey:@"msg"];
-        //        NSString *code = [resultDict objectForKey:@"code"];
-        if ([success boolValue]) {
-            NSDictionary *data = [resultDict objectForKey:@"data"];
-            if (data != nil) {
-                self.dataSource = [NSMutableArray arrayWithObject:data];
-//                [self.mytableview reloadData];
-                [self loadDataPingLun];
-            }else{
-                [HUD hide:YES];
-            }
-        }else{
-            [HUD hide:YES];
-        }
-    }errorHandler:^(MKNetworkOperation *errorOp, NSError* err) {
-        NSLog(@"MKNetwork request error : %@", [err localizedDescription]);
-        [HUD hide:YES];
-        
-    }];
-    [engine enqueueOperation:op];
 }
 
 //加载评论
@@ -378,7 +314,6 @@
                 }
                 [self.mytableview reloadData];
             }
-            
         }else{
         }
     }errorHandler:^(MKNetworkOperation *errorOp, NSError* err) {
@@ -432,10 +367,7 @@
     }else{
         [self.mytableview.footer endRefreshing];
     }
-    
 }
-
-
 
 #pragma mark - Table view data source
 
@@ -444,124 +376,60 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//    if ([page intValue]!= [totalpage intValue] && [self.dataSource count] != 1) {
-//        return [[self dataSource] count] + 1;
-//    }else{
-        return [[self dataSource] count];
-//    }
+    return [[self dataSource] count];
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"pingluncell";
+    PinglunTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[[NSBundle mainBundle] loadNibNamed:@"PinglunTableViewCell" owner:self options:nil] lastObject];
+        NSLog(@"新建cell");
+    }
+    NSDictionary *data = [self.dataSource objectAtIndex:indexPath.row];
+    NSString *commentContent = [data objectForKey:@"commentContent"];
+    NSString *fileid = [data objectForKey:@"fileid"];
+    NSString *userName = [data objectForKey:@"userName"];
+    NSString *commentDate = [data objectForKey:@"commentDate"];
     
-//    if ([indexPath row] == 0) {
-//        static NSString *cellIdentifier = @"contentcell";
-//        ContentCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-//        if (!cell) {
-//            cell = [[[NSBundle mainBundle] loadNibNamed:@"ContentCell" owner:self options:nil] lastObject];
-//        }
-//        NSDictionary *data = [self.dataSource objectAtIndex:indexPath.row];
-//
-//        NSString *title = [data objectForKey:@"tntitle"];
-//        NSString *date = [data objectForKey:@"tnmoddate"];
-//        NSString *content = [data objectForKey:@"tncontent"];
-//        cell.contentTitle.text = title;
-//        cell.contentDate.text = date;
-//        cell.content.text = content;
-//        //    cell.content.text = @"测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试";
-//        cell.content.numberOfLines = 0;
-//        [cell.content sizeToFit];
-//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-//        
-//        CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width ;
-//        UIFont *font = [UIFont systemFontOfSize:17];
-//        CGSize size = [content sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth-16, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
-//        [cell.content setFrame:CGRectMake(cell.content.frame.origin.x, cell.content.frame.origin.y, cell.content.frame.size.width, size.height)];
-//        return cell;
-//        
-//    }else{
-//        if ([self.dataSource count] == indexPath.row) {
-//            static NSString *cellIdentifier = @"morecell";
-//            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-//            if (!cell) {
-//                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
-//                cell.textLabel.text = @"点击加载更多";
-//            }
-//            cell.textLabel.textAlignment = NSTextAlignmentCenter;
-//            return cell;
-//            
-//        }else{
-            static NSString *cellIdentifier = @"pingluncell";
-            PinglunTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-            if (!cell) {
-                cell = [[[NSBundle mainBundle] loadNibNamed:@"PinglunTableViewCell" owner:self options:nil] lastObject];
-                NSLog(@"新建cell");
-            }
-            NSDictionary *data = [self.dataSource objectAtIndex:indexPath.row];
-            NSString *commentContent = [data objectForKey:@"commentContent"];
-            NSString *fileid = [data objectForKey:@"fileid"];
-            NSString *userName = [data objectForKey:@"userName"];
-            NSString *commentDate = [data objectForKey:@"commentDate"];
-            
-            cell.namelabel.text = userName;
-            [cell.namelabel setTextColor:[UIColor colorWithRed:42/255.0 green:173/255.0 blue:128/255.0 alpha:1]];
-            cell.datelabel.text = commentDate;
-            
-            
-            
-            //评论内容 高度自适应
-            cell.commentlabel.text = commentContent;
-            cell.commentlabel.numberOfLines = 0;
-            cell.commentlabel.lineBreakMode = NSLineBreakByCharWrapping;
-            [cell.commentlabel sizeToFit];
-            cell.selectionStyle = UITableViewCellSelectionStyleNone;
-            CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width ;
-            UIFont *font = [UIFont systemFontOfSize:14];
-            CGSize size = [commentContent sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth-59, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
-            [cell.commentlabel setFrame:CGRectMake(cell.commentlabel.frame.origin.x, cell.commentlabel.frame.origin.y, cell.commentlabel.frame.size.width, size.height)];
-            
-            
-            
-            if ([Utils isBlankString:fileid]) {
-                [cell.img setImage:[UIImage imageNamed:@"chatListCellHead.png"]];
-            }else{
-                [cell.img setImageWithURL:[NSURL URLWithString:fileid] placeholderImage:[UIImage imageNamed:@"chatListCellHead.png"]];
-            }
-            cell.selectionStyle = UITableViewCellSelectionStyleNone;
-            return cell;
-//        }
-//    }
+    cell.namelabel.text = userName;
+    [cell.namelabel setTextColor:[UIColor colorWithRed:42/255.0 green:173/255.0 blue:128/255.0 alpha:1]];
+    cell.datelabel.text = commentDate;
+    
+    //评论内容 高度自适应
+    cell.commentlabel.text = commentContent;
+    cell.commentlabel.numberOfLines = 0;
+    cell.commentlabel.lineBreakMode = NSLineBreakByCharWrapping;
+    [cell.commentlabel sizeToFit];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width ;
+    UIFont *font = [UIFont systemFontOfSize:14];
+    CGSize size = [commentContent sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth-59, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
+    [cell.commentlabel setFrame:CGRectMake(cell.commentlabel.frame.origin.x, cell.commentlabel.frame.origin.y, cell.commentlabel.frame.size.width, size.height)];
+    
+    
+    
+    if ([Utils isBlankString:fileid]) {
+        [cell.img setImage:[UIImage imageNamed:@"chatListCellHead.png"]];
+    }else{
+        [cell.img setImageWithURL:[NSURL URLWithString:fileid] placeholderImage:[UIImage imageNamed:@"chatListCellHead.png"]];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     NSInteger row = [indexPath row];
-//    if (row == 0) {
-//        NSInteger row = [indexPath row];
-//        // 列寬
-//        CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width;
-//        // 用何種字體進行顯示
-//        UIFont *font = [UIFont systemFontOfSize:17];
-//        // 該行要顯示的內容
-//        NSString *content = [[self.dataSource objectAtIndex:row] objectForKey:@"tncontent"];
-//        //    NSString *content = @"测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试";
-//        // 計算出顯示完內容需要的最小尺寸
-//        CGSize size = [content sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth-16, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
-//        return size.height+86;
-//    }else{
-//        if ([self.dataSource count] == indexPath.row) {
-//            return 44;
-//        }else{
-            // 列寬
-            CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width-59;
-            // 用何種字體進行顯示
-            UIFont *font = [UIFont systemFontOfSize:14];
-            // 該行要顯示的內容
-            NSString *content = [[self.dataSource objectAtIndex:row] objectForKey:@"commentContent"];
-            // 計算出顯示完內容需要的最小尺寸
-            CGSize size = [content sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
-            return size.height+60;
-//        }
-//    }
+    // 列寬
+    CGFloat contentWidth = [UIScreen mainScreen].bounds.size.width-59;
+    // 用何種字體進行顯示
+    UIFont *font = [UIFont systemFontOfSize:14];
+    // 該行要顯示的內容
+    NSString *content = [[self.dataSource objectAtIndex:row] objectForKey:@"commentContent"];
+    // 計算出顯示完內容需要的最小尺寸
+    CGSize size = [content sizeWithFont:font constrainedToSize:CGSizeMake(contentWidth, MAXFLOAT) lineBreakMode:NSLineBreakByCharWrapping];
+    return size.height+60;
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -572,24 +440,6 @@
         [cell setLayoutMargins:UIEdgeInsetsZero];
     }
 }
-
-//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-////    if ([self.dataSource count] != 1) {
-//        if ([self.dataSource count] == indexPath.row) {
-//            if (page == totalpage) {
-//                
-//            }else{
-//                [HUD show:YES];
-//                [self loadDataPingLunMore];
-//            }
-//        }else{
-//            
-//        }
-////    }
-//    
-//    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-//}
-
 //提示
 - (void)alertMsg:(NSString *)msg{
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
@@ -604,33 +454,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-//{
-//    [_slimeView scrollViewDidScroll];
-//}
-//
-//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
-//{
-//    [_slimeView scrollViewDidEndDraging];
-//}
-
-#pragma mark - slimeRefresh delegate
-//刷新消息列表
-- (void)slimeRefreshStartRefresh:(SRRefreshView *)refreshView
-{
-    
-    [self loadDataPingLun];
-    [_slimeView endRefresh];
-}
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
